3250c0e98e32ce9888d0918aedce5fc9ebd530a3
Pirashan <pirashan99@gmail.com>
2022-10-27 00:27:11 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7MV2Vrp4GiiFFryDyQYPQsr1SNHouaIM",
+    authDomain: "netflix-clone-27d61.firebaseapp.com",
+    projectId: "netflix-clone-27d61",
+    storageBucket: "netflix-clone-27d61.appspot.com",
+    messagingSenderId: "634709386088",
+    appId: "1:634709386088:web:313b81466af250e70d6cdd"
+  };
